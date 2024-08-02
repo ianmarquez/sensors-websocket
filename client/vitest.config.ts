448425bc3a87feb66991197400d1b24370d225f1
Ian Marquez <regilmarquez123@gmail.com>
2024-08-02 20:19:05 +0800

    Update: added vitest

--- client/vitest.config.ts
+++ client/vitest.config.ts
@@ -2,7 +2,18 @@
 
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
-import { defineConfig } from "vitest/config";
+import { defineConfig, configDefaults } from "vitest/config";
+
+const exclude = [
+  ...configDefaults.exclude,
+  "**/node_modules/**",
+  "**/dist/**",
+  "**/cypress/**",
+  "**/.{idea,git,cache,output,temp}/**",
+  "**/*.config.js",
+  "**/*.d.ts",
+  "**/*.cjs",
+];
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -12,5 +23,10 @@ export default defineConfig({
     environment: "jsdom",
     include: ["**/*.test.tsx"],
     setupFiles: "./src/test/setup.ts",
+    exclude,
+    coverage: {
+      provider: "v8",
+      exclude: [...exclude, "**/*.test.*"],
+    },
   },
 });
